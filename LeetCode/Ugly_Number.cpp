d3fc0970fa8754f6d28bbe2f0c04661b4e9c8e88
Schrödinger‘s koneko <HatsuneMoe@users.noreply.github.com>
2017-07-11 14:35:54 +0800

    Fix WA bug by 0 and Add a new version solution

--- LeetCode/Ugly_Number.cpp
+++ LeetCode/Ugly_Number.cpp
@@ -13,6 +13,8 @@ public:
         // Write your code here
         if (num == 1)
             return true;
+        if (num == 0)    
+            return false;
         while(!(num & 1))
             num = num >>1;
 
@@ -25,6 +27,16 @@ public:
         }
 
         return num == 1;
+    }
+};
+
+class Solution_another {
+public:
+    bool isUgly(int num) {
+		for (int i = 2; i<6 && num; i++)
+			while (num % i == 0)
+				num /= i;
+		return num == 1;
     }
 };
 
